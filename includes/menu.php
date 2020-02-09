a296187fe0511328193e6d126085d4e6dd682316
roberto103 <42358389+roberto103@users.noreply.github.com>
2020-02-09 11:20:47 -0300

    Update menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -42,7 +42,7 @@
           </a>
           <ul class="nav nav-treeview">
             <li class="nav-item">
-              <a href="pages/charts/chartjs.html" class="nav-link">
+              <a href="produtos.php" class="nav-link">
                 <i class="far fa-circle nav-icon"></i>
                 <p>Produtos</p>
               </a>

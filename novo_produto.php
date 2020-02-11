b2f20a86390dd203202f91e8d7c5da969677cba3
MatteuSilva <42352998+MatteuSilva@users.noreply.github.com>
2020-02-11 19:21:28 -0300

    place holder

--- novo_produto.php
+++ novo_produto.php
@@ -31,39 +31,39 @@
         <div class="col-sm-8">
           <div class="row" style="margin-left:0.1%;">
             <label for="nome_cliente">Modelo</label>
-            <input id="nome_cliente" name="nome_cliente" type="name" value="" class="form-control">
+            <input id="nome_cliente" name="nome_cliente" type="name" value="" class="form-control" placeholder="Modelo do produto">
           </div>
           <br>
           <div class="row">
             <div class="col">
               <label for="">Preço de compra</label>
-              <input id="data_nascimento" name="data_nascimento" type="number" class="form-control" placeholder="Data de nascimento">
+              <input id="data_nascimento" name="data_nascimento" type="number" class="form-control" placeholder="Preço de compra">
             </div>
             <div class="col">
               <label for="data_cliente">Preço de venda</label>
-              <input id="data_cliente" name="data_cliente" type="number" class="form-control" placeholder="Inicio da fidelidade">
+              <input id="data_cliente" name="data_cliente" type="number" class="form-control" placeholder="Preço de venda">
             </div>
           </div>
           <br>
           <div class="row">
             <div class="col">
               <label for="cidade_cliente">Fabricante</label>
-              <input id="cidade_cliente" name="cidade_cliente" type="text" class="form-control" placeholder="Cidade">
+              <input id="cidade_cliente" name="cidade_cliente" type="text" class="form-control" placeholder="Fabricante do produto">
             </div>
             <div class="col">
               <label for="bairro_cliente">IMAC</label>
-              <input id="bairro_cliente" name="bairro_cliente" type="text" class="form-control" placeholder="Bairro">
+              <input id="bairro_cliente" name="bairro_cliente" type="text" class="form-control" placeholder="IMAC">
             </div>
           </div>
           <br>
           <div class="row">
             <div class="col">
               <label for="estado_produto">Estado</label>
-              <input id="estado_produto" name="estado_produto" type="text" class="form-control" placeholder="Cidade">
+              <input id="estado_produto" name="estado_produto" type="text" class="form-control" placeholder="Novo, semi-novo ...">
             </div>
             <div class="col">
               <label for="quantidade_produto">Quantidade</label>
-              <input id="quantidade_produto" name="quantidade_produto" type="text" class="form-control" placeholder="Cidade">
+              <input id="quantidade_produto" name="quantidade_produto" type="text" class="form-control" placeholder="Quantidade do produto">
             </div>
           </div>
           <br>

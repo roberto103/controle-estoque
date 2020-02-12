038c55444fb0c8d2a286cb486e766d60e1940107
roberto103 <42358389+roberto103@users.noreply.github.com>
2020-02-12 09:22:53 -0300

    Update menu.php

--- includes/menu.php
+++ includes/menu.php
@@ -7,18 +7,6 @@
     <span class="brand-text font-weight-light">Estoque</span>
   </a>
 
-  <!-- Sidebar -->
-  <div class="sidebar">
-    <!-- Sidebar user panel (optional) -->
-    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-      <div class="image">
-        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-      </div>
-      <div class="info">
-        <a href="#" class="d-block">Usuário</a>
-      </div>
-    </div>
-
     <!-- Sidebar Menu -->
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">

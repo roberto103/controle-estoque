f3de1d1aace0bad7bb57e883d5a796ec1d38ad41
roberto103 <42358389+roberto103@users.noreply.github.com>
2020-02-13 18:11:51 -0300

    produtos

--- nova_venda.php
+++ nova_venda.php
@@ -27,70 +27,36 @@
   <!-- Main content -->
 
   <div class="container-fluid">
-    <form action="" method="POST">
 
+    <div class="row">
       <div class="col-sm-8">
-        <div class="row mb-3">
-          <div class="col">
-            <label>Fabricante</label>
-            <select class="form-control">
-              <option>Apple</option>
-            </select>
+        <div class="container">
+
+          <div class="card d-inline-block" style="width: 18rem; margin-right: 25px;">
+            <img src="img/iphone-xr.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title mb-3">iPhone XR 128GB</h5>
+              <p class="card-text mb-3">R$ 4.599,00</p>
+              <a href="#" class="btn btn-primary float-right">Vender</a>
+            </div>
           </div>
 
-          <div class="col">
-            <label>Modelo</label>
-            <select class="form-control">
-              <option>iPhone X</option>
-              <option>iPhone XR</option>
-              <option>iPhone XR 128GB</option>
-            </select>
+          <div class="card d-inline-block" style="width: 18rem; margin-right: 25px;">
+            <img src="img/iphone-xr-red.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title mb-3">iPhone XR 128GB</h5>
+              <p class="card-text mb-3">R$ 4.599,00</p>
+              <a href="#" class="btn btn-primary float-right">Vender</a>
+            </div>
           </div>
+
         </div>
-
-        <!-- <div class="row mb-3">
-        <div class="col">
-        <label for="preco_compra">Preço de compra</label>
-        <input id="preco_compra" name="preco_compra" type="text" class="form-control money" placeholder="Preço de compra" required>
-      </div>
-      <div class="col">
-      <label for="preco_venda">Preço de venda</label>
-      <input id="preco_venda" name="preco_venda" type="text" class="form-control money" placeholder="Preço de venda" required>
-    </div>
-  </div> -->
-
-  <div class="row mb-3">
-    <div class="col">
-      <label for="imac">IMAC</label>
-      <input id="imac" name="imac" type="text" class="form-control" placeholder="IMAC" required>
-    </div>
-  </div>
-
-  <div class="row mb-3">
-    <div class="col">
-      <div class="form-group">
-        <label>Estado</label>
-        <select class="form-control">
-          <option>Novo</option>
-          <option>Seminovo</option>
-          <option>Usado</option>
-        </select>
       </div>
     </div>
 
-    <div class="col">
-      <label for="quantidade_produto">Quantidade</label>
-      <input id="quantidade_produto" name="quantidade_produto" type="number" class="form-control" placeholder="Quantidade do produto" required>
-    </div>
+    <div class="col-sm-4"></div>
+
   </div>
-
-  <button type="submit" class="btn btn-success">Realizar venda</button>
-</div>
-</form>
-
-<div class="col-sm-4"></div>
-
-</div>
 </div>
 
 <?php include_once 'includes/footer.php'; ?>

db8f141ce822ea1b30b8e231a3edde6e616b87cc
roberto103 <42358389+roberto103@users.noreply.github.com>
2020-03-02 13:12:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,105 +16,97 @@ $user = $pdo->prepare('SELECT * FROM usuarios');
 $user->execute();
 $usuarios = $user->rowCount();
 
-$valor_venda = $pdo->prepare('SELECT SUM(valor_venda) AS vendas_total FROM produtos_vendidos');
-$valor_venda->execute();
-$venda_total = $valor_venda->fetchAll(PDO::FETCH_OBJ);
+?>
 
-foreach ($venda_total as $venda_total);
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0 text-dark">Dashboard</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item">
+              <a href="/">Início</a>
+            </li>
+            <li class="breadcrumb-item active">Dashboard</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /.content-header -->
 
-$total_vendas = $venda_total->vendas_total;
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-3 col-6">
+          <div class="small-box bg-info">
+            <div class="inner">
+              <h3><?php echo $produtos_vendido; ?></h3>
 
- ?>
-
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-      <!-- Content Header (Page header) -->
-      <div class="content-header">
-        <div class="container-fluid">
-          <div class="row mb-2">
-            <div class="col-sm-6">
-              <h1 class="m-0 text-dark">Dashboard</h1>
+              <p>Vendas</p>
             </div>
-            <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item">
-                  <a href="/">Início</a>
-                </li>
-                <li class="breadcrumb-item active">Dashboard</li>
-              </ol>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
             </div>
+            <a href="#" class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+          <div class="small-box bg-success">
+            <div class="inner">
+              <h3>R$ <?php echo $lucro; ?><sup style="font-size: 20px"></sup></h3>
+
+              <p>de lucro</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+          <div class="small-box bg-warning">
+            <div class="inner">
+              <h3><?php echo $linhas; ?></h3>
+
+              <p>Clientes cadastrados</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+          <div class="small-box bg-danger">
+            <div class="inner">
+              <h3><?php echo $usuarios; ?></h3>
+
+              <p>Usuários</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person"></i>
+            </div>
+            <a href="#" class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
       </div>
-      <!-- /.content-header -->
 
-      <!-- Main content -->
-      <section class="content">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-lg-3 col-6">
-              <div class="small-box bg-info">
-                <div class="inner">
-                  <h3><?php echo $produtos_vendido; ?></h3>
+      <div class="row">
 
-                  <p>Vendas</p>
-                </div>
-                <div class="icon">
-                  <i class="ion ion-bag"></i>
-                </div>
-                <a href="#" class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
-              </div>
-            </div>
+      </div>
 
-            <div class="col-lg-3 col-6">
-              <div class="small-box bg-success">
-                <div class="inner">
-                  <h3>R$ <?php echo $total_vendas; ?><sup style="font-size: 20px"></sup></h3>
-
-                  <p>de lucro</p>
-                </div>
-                <div class="icon">
-                  <i class="ion ion-stats-bars"></i>
-                </div>
-                <a href="#" class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
-              </div>
-            </div>
-
-            <div class="col-lg-3 col-6">
-              <div class="small-box bg-warning">
-                <div class="inner">
-                  <h3><?php echo $linhas; ?></h3>
-
-                  <p>Clientes cadastrados</p>
-                </div>
-                <div class="icon">
-                  <i class="ion ion-person-add"></i>
-                </div>
-                <a href="#" class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
-              </div>
-            </div>
-
-            <div class="col-lg-3 col-6">
-              <div class="small-box bg-danger">
-                <div class="inner">
-                  <h3><?php echo $usuarios; ?></h3>
-
-                  <p>Usuários</p>
-                </div>
-                <div class="icon">
-                  <i class="ion ion-person"></i>
-                </div>
-                <a href="#" class="small-box-footer">Mais informações <i class="fas fa-arrow-circle-right"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-
-          </div>
-
-        </div>
-      </section>
     </div>
+  </section>
+</div>
 
-    <?php include_once 'includes/footer.php'; ?>
+<?php include_once 'includes/footer.php'; ?>

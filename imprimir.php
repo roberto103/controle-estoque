50c8701838d0d40a79d97176e5c2247c19d966ce
roberto103 <42358389+roberto103@users.noreply.github.com>
2020-03-04 18:27:14 -0300

    Imprimir comprovante de pagamento (PRECISA DO NOME DO CLIENTE E DO VENDEDOR)

---
+++ imprimir.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once 'core/conexao.php';
+require_once 'core/Sessao.php';
+require_once 'core/util.php';
+
+if (!Sessao::estaLogado()) {
+  header('Location: login.php');
+}
+
+$sql = $pdo->prepare('SELECT * FROM produtos_vendidos WHERE id = :id');
+$sql->bindvalue(':id', $_GET['id']);
+$sql->execute();
+$registro = $sql->fetchAll(PDO::FETCH_OBJ);
+
+foreach ($registro as $registros);
+
+?>
+
+<script type="text/javascript">
+  window.print();
+</script>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Comprovante de Venda - Controle de Estoque</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Tempusdominus Bbootstrap 4 -->
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</style>
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper">
+    <section class="content">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 pt-5">
+
+            <div class="row">
+              <div class="col" style="margin-bottom: 35px;">
+                <h5 class="d-block">Cliente: <?php echo $registros->cliente; ?></h5>
+                <h5 class="d-block">Vendedor: <?php echo $registros->vendedor; ?></h5>
+              </div>
+
+              <div class="col text-right" style="margin-bottom: 35px;">
+                <h5 class="d-block">Data: <?php echo dataHoraTela($registros->data); ?></h5>
+                <h5 class="d-block">ID da venda: <?php echo $registros->id; ?></h5>
+              </div>
+            </div>
+
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Produto</th>
+                  <th>Estado</th>
+                  <th>Valor</th>
+                  <th>Número de Série</th>
+                  <th>IMEI</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><?php echo $registros->modelo; ?></td>
+                  <td><?php echo $registros->estado_produto; ?></td>
+                  <td>R$ <?php echo decimalTela($registros->valor_venda); ?></td>
+                  <td><?php echo $registros->numero_serie; ?></td>
+                  <td><?php echo $registros->imei; ?></td>
+                </tr>
+              </tbody>
+            </table>
+
+          </div>
+        </div>
+      </div>
+
+    </section>
+  </div>
+</body>

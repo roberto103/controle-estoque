6ca2975b1738fd9dcf84a86acd576f944e973f88
roberto103 <42358389+roberto103@users.noreply.github.com>
2020-03-03 23:05:53 -0300

    Revert "Removi arquivos que n estavam sendo usandos"
    This reverts commit 4ad60d76d3f2db3c0c937184a769c2bccd56e070.

--- includes/footer.php
+++ includes/footer.php
@@ -29,31 +29,50 @@ $.widget.bridge('uibutton', $.ui.button)
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- ChartJS -->
 <!-- <script src="plugins/chart.js/Chart.min.js"></script> -->
+<!-- Sparkline -->
+<!-- <script src="plugins/sparklines/sparkline.js"></script> -->
+<!-- JQVMap -->
+<!-- <script src="plugins/jqvmap/jquery.vmap.min.js"></script> -->
+<!-- <script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script> -->
+<!-- jQuery Knob Chart -->
+<!-- <script src="plugins/jquery-knob/jquery.knob.min.js"></script> -->
+<!-- daterangepicker -->
+<!-- <script src="plugins/moment/moment.min.js"></script> -->
+<!-- <script src="plugins/daterangepicker/daterangepicker.js"></script> -->
+<!-- Tempusdominus Bootstrap 4 -->
+<!-- <script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script> -->
+<!-- Summernote -->
+<!-- <script src="plugins/summernote/summernote-bs4.min.js"></script> -->
+<!-- overlayScrollbars -->
+<!-- <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script> -->
 <!-- DataTables -->
 <script src="plugins/datatables/jquery.dataTables.js"></script>
 <!-- <script src="datatables-bs4/js/dataTables.bootstrap4.js"></script> -->
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<!-- <script src="dist/js/pages/dashboard.js"></script> -->
 <!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
 
 <!-- page script -->
 <script>
+
 $(document).ready(function(){
   $(".phone").mask("(00) 0 0000-0000");
   $('.money').mask('000.000.000.000.000,00', {reverse: true});
 });
 
-$(function () {
-  $('#example2').DataTable({
-    "paging": false,
-    "lengthChange": false,
-    "searching": true,
-    "ordering": true,
-    "info": true,
-    "autoWidth": false,
+  $(function () {
+    $('#example2').DataTable({
+      "paging": false,
+      "lengthChange": false,
+      "searching": true,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+    });
   });
-});
 </script>
 
 </body>

3da7499aa6e6ee44ce97a31431a5d0f208a533f6
roberto103 <42358389+roberto103@users.noreply.github.com>
2020-04-22 09:43:24 -0300

    Função para estonar produtos funcionando

--- core/estornar.php
+++ core/estornar.php
@@ -1,16 +1,23 @@
 <?php
 
-
 require_once 'conexao.php';
-
-
 session_start();
-//deletar
-$sql = $pdo->prepare('DELETE FROM produtos WHERE id = :id');
+
+$sql = $pdo->prepare('INSERT INTO produtos (modelo, estado_produto, valor_compra, valor_venda, numero_serie, imei, data_compra)
+  SELECT modelo, estado_produto, valor_compra, valor_venda, numero_serie, imei, data_compra
+  FROM produtos_vendidos WHERE id = :id');
 $sql->bindValue(':id', $_GET['id']);
 $sql->execute();
 
-//adicionar
+// Delete
+$delete = $pdo->prepare('DELETE FROM produtos_vendidos WHERE id = :id');
+$delete->bindValue(':id', $_GET['id']);
+$delete->execute();
 
-
-?>
+if ($sql && $delete) {
+  $_SESSION['msg'] = '<div class="alert alert-success" role="alert">Produto estornado!</div>';
+  header('Location: ../historico_vendas.php');
+} else {
+  $_SESSION['msg'] = '<div class="alert alert-danger" role="alert">Falha ao estornar produto!</div>';
+  header('Location: ../historico_vendas.php');
+}
--- historico_vendas.php
+++ historico_vendas.php
@@ -79,7 +79,7 @@ $registros = $sql->fetchAll(PDO::FETCH_OBJ);
                       <td><?php echo dataTela($produtos->data_venda); ?></td>
                       <td><?php echo $produtos->vendedor; ?></td>
                       <td class="text-center">
-                        <a href="estornar.php?id=<?php echo $produtos->id; ?>" title="Estornar" style="font-size: 20px;">
+                        <a href="core/estornar.php?id=<?php echo $produtos->id; ?>" title="Estornar" style="font-size: 20px;">
                           <i class="fas fa-retweet"></i>
                         </a>
                       </td>
